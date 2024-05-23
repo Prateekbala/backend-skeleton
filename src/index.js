4bf939b97f18754f99c480be407fab9ccbe5eca7
Prateek Bala <prateekbala28@gmail.com>
2024-05-23 15:47:12 +0530

    set up

--- src/index.js
+++ src/index.js
@@ -6,3 +6,32 @@ app.listen(8000,()=>{
     console.log('App is listening on port 8000');
     
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//"dev": "nodemon -r dotenv/config --experimental-json-modules src/index.js"
