892dc4638d286ed482dab8fcf319d1224de5632f
Prateek Bala <prateekbala28@gmail.com>
2024-05-23 17:32:37 +0530

    Database setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import express from "express"
+import express from "express";
+import connectDB from "../DB/index.js"; 
+const app = express();
+console.log('done');
 
-const app=express()
-
-app.listen(8000,()=>{
+app.listen(8000, () => {
     console.log('App is listening on port 8000');
-    
-})
+});
 
-
-
+connectDB(); 
 
 
 
